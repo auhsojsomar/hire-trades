99c2119d50261bbfb8ef2815b5304c4c08d81888
Joshua Ramos <auhsojsomar@gmail.com>
2021-04-08 12:01:00 +0800

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="navbar">
+    <div class="nav-item">
+      <div class="logo">
+        <img src="../assets/images/logo-brand.png" alt="logo-brand" />
+        <img src="../assets/images/logo-name.png" alt="logo-name" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    title: String,
+  },
+  methods: {
+    onClick() {
+      alert("onClick");
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.navbar
+  border-bottom: 1px solid #dfe3e7;
+  .nav-item
+    padding: 0px 15px;
+    margin: auto
+    .logo
+      display: inline-block
+      padding: 10px 0
+      margin: auto
+      img:last-child
+        margin: 0 0 5px 10px
+</style>
